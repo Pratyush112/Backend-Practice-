a962522678ffb28702501fe67f7080620d298bc7
Pratyush112 <prasaha003@gmail.com>
2025-12-24 19:48:33 +0530

    updated some files

---
+++ src/server.js
@@ -0,0 +1,14 @@
+import dotenv from 'dotenv';
+import connectDatabse from './db/db.js';
+import express from 'express';
+
+dotenv.config({
+    path: './.env'
+})
+
+const app = express();
+connectDatabse();
+.then(
+app.listen(process.env.PORT || 8000, ()=> {
+    console.log(`Server is runnning on port http://localhost:${process.env.PORT}`)
+}))
